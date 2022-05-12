571d45a1a5094e843935efb6f1252aad9a9b51ef
Hoang Vu Nguyen <hoangvuconco@gmail.com>
2022-05-12 12:35:19 +0700

    remove zip data from git repo

--- tests/test_gene_db.py
+++ tests/test_gene_db.py
@@ -2,8 +2,6 @@ import tempfile
 from walnut.gene_db import GeneDB
 from walnut.study import Study
 from walnut import common
-import os, pathlib, tarfile
-import scanpy as sc
 
 def test_gene_db_conversion():
     gene_db = GeneDB(common.get_pkg_data(), "human")
@@ -28,21 +26,11 @@ def test_gene_db_creation():
 
 def test_gene_db_creation_with_dup():
     # Prepare data
-    current_dir = os.path.dirname(pathlib.Path(__file__))
-    data_path = os.path.join(current_dir, 'test_data/pbmc3k_filtered_gene_bc_matrices.tar.gz')
-    file = tarfile.open(data_path)
-    tmp_dir = tempfile.mkdtemp()
-
-    file.extractall(tmp_dir)
-    file.close()
-
-    adata = sc.read_10x_mtx(
-        os.path.join(tmp_dir, 'filtered_gene_bc_matrices/hg19/'),
-        var_names='gene_symbols',
-        cache=True)
-
-    features = adata.var_names.tolist()
-
+    
+    gene_db = GeneDB(common.get_pkg_data(), "human")
+    gene_db.read()
+    df = gene_db.to_df()
+    features = df['gene_id'][0:10000]
 
     study_folder = tempfile.mkdtemp()
     study = Study(study_folder)
